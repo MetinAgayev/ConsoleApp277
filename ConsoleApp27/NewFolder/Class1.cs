5bf7e79ad9759f6e67651aad93229a77954415d6
MetinAgayev <matinga@code.edu.az>
2021-12-19 22:56:47 +0400

    Add project files.

---
+++ ConsoleApp27/NewFolder/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp27.NewFolder
+{
+   public static class Class1
+    {
+        public static bool Word(this string word,string hissecik)
+        {
+            StringBuilder sb = new StringBuilder(hissecik);
+            string[] letter = word.Split(' ');
+            StringBuilder sb1 = new StringBuilder(word);
+            foreach (var item in word)
+            {
+
+                foreach (var item1 in hissecik)
+                {
+                    if (item1 == item)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+            
+        }
+    }
+}
---
+++ ConsoleApp27/Program.cs
@@ -0,0 +1,14 @@
+﻿using ConsoleApp27.NewFolder;
+using System;
+
+namespace ConsoleApp27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "salm dostlar";
+            word.Word("dosq");
+        }
+    }
+}
